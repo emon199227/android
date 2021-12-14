9afe85efe7eb5940aa98dc54c2fc0d2a4a410426
unknown <emon199227@gmail.com>
2021-12-15 02:58:47 +0600

    spalsh screen or welcome screen

--- ProgressBar/app/src/main/java/info/emon/MainActivity.java
+++ ProgressBar/app/src/main/java/info/emon/MainActivity.java
@@ -3,6 +3,8 @@ package info.emon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import java.util.Timer;
@@ -15,7 +17,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove the notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
 
         prog();
     }
