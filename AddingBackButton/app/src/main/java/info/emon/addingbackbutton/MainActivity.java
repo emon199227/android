2f42f5ffd89eb2a6a7531b679a7e65e739aa2338
unknown <emon199227@gmail.com>
2022-01-24 00:49:08 +0600

    add back button

--- AddingBackButton/app/src/main/java/info/emon/addingbackbutton/MainActivity.java
+++ AddingBackButton/app/src/main/java/info/emon/addingbackbutton/MainActivity.java
@@ -1,59 +1,45 @@
-package info.emon.embedwebsie;
+package info.emon.addingbackbutton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView website;
-    private ImageView logoid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        website=(WebView)findViewById(R.id.webviewID);
-        logoid=(ImageView)findViewById(R.id.logo);
+        website = (WebView) findViewById(R.id.webviewID);
 
-
-
-        logoid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Mathbaria News : : Develop By Emon :: Contact : 01786575404",Toast.LENGTH_LONG).show();
-            }
-        });
-
-        WebSettings webSettings=website.getSettings();
+        WebSettings webSettings = website.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        //adding back button on the tool bar
 
-    //adding back button on the tool bar
-
-       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         //end calling back button ,now write code on out of oncreate option""""""""""""""
+
         website.setWebViewClient(new WebViewClient());
 
         website.loadUrl("http://prothomalo.com");
+
     }
 
- /*   @Override
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==android.R.id.home)
-        {
+        if (item.getItemId() == android.R.id.home) {
             this.finish();
         }
         return super.onOptionsItemSelected(item);
-    }*/
-
+    }
     @Override
     public void onBackPressed() {
         if(website.canGoBack()){
